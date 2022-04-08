278121e17b4db735004b4dfec89c70e14b6fec15
dmytro <d.u.zheliabin@gmail.com>
2022-04-08 16:11:38 +0300

    add style

--- auctions/forms.py
+++ auctions/forms.py
@@ -8,12 +8,17 @@ class ListingForm(ModelForm):
         model = AuctionListing
         fields = ['title', 'description', 'current_bid', 'image', 'category']
 
+        labels = {
+            'current_bid': 'Starting bid',
+            'image': 'Image URL'
+        }
+
         widgets = {
-            'title': TextInput(attrs={'class': 'form-control w-75'}),
-            'description': Textarea(attrs={'class': 'form-control w-75'}),
-            'current_bid': NumberInput(attrs={'class': 'form-control w-75'}),
-            'image': URLInput(attrs={'class': 'form-control w-75'}),
-            'category': Select(attrs={'class': 'form-control w-75'}),
+            'title': TextInput(attrs={'class': 'form-control'}),
+            'description': Textarea(attrs={'class': 'form-control'}),
+            'current_bid': NumberInput(attrs={'class': 'form-control'}),
+            'image': URLInput(attrs={'class': 'form-control'}),
+            'category': Select(attrs={'class': 'form-control'}),
         }
 
 
@@ -26,5 +31,5 @@ class BidForm(ModelForm):
             'bid': ''
         }
         widgets = {
-            'bid': NumberInput(attrs={'class': 'form-control w-75', 'placeholder': 'Bid'})
+            'bid': NumberInput(attrs={'class': 'form-control', 'placeholder': 'Bid'})
         }
